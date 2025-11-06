3c2e1a74359e4e347de59b45ba9b99a017d35c98
Modesto Castrillón (División de Robótica y Oceanografía Computacional - SIANI - ULPGC) <otsedom@gmail.com>
2025-11-06 11:11:32 +0000

    S9 publicada

---
+++ IG/S9/code/script_31_shadermin.js
@@ -0,0 +1,97 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+let scene, renderer;
+let camera;
+let info;
+let grid;
+let camcontrols1;
+let objetos = [];
+
+init();
+animationLoop();
+
+function init() {
+  //Defino cámara
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 0, 5);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //Objetos
+  Esfera(-1.5, 0, 0, 0.8, 10, 10, 0xff0000);
+  EsferaShader(1.5, 0, 0, 0.8, 10, 10);
+
+  //Orbit control y rejilla
+  camcontrols1 = new OrbitControls(camera, renderer.domElement);
+  grid = new THREE.GridHelper(20, 40);
+  grid.geometry.rotateX(Math.PI / 2);
+  grid.position.set(0, 0, 0.05);
+  scene.add(grid);
+}
+
+function Esfera(px, py, pz, radio, nx, ny, col) {
+  let geometry = new THREE.SphereBufferGeometry(radio, nx, ny);
+  let material = new THREE.MeshBasicMaterial({
+    color: col,
+  });
+
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(px, py, pz);
+  scene.add(mesh);
+  objetos.push(mesh);
+}
+
+function vertexShader() {
+  return `
+				varying vec3 vUv; 
+				varying vec4 modelViewPosition; 
+				
+				void main() {
+				  vUv = position; 
+				  vec4 modelViewPosition = modelViewMatrix * vec4(vUv, 1.0);
+				  gl_Position = projectionMatrix * modelViewPosition; 
+				}
+			  `;
+}
+
+function fragmentShader() {
+  return `
+				  void main() {
+					  gl_FragColor = vec4(0.331,0.067,0.300,1.0);
+				  }
+			  `;
+}
+
+function EsferaShader(px, py, pz, radio, nx, ny) {
+  let geometry = new THREE.SphereGeometry(radio, nx, ny);
+  let material = new THREE.ShaderMaterial({
+    fragmentShader: fragmentShader(),
+    vertexShader: vertexShader(),
+  });
+
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(px, py, pz);
+  scene.add(mesh);
+  objetos.push(mesh);
+}
+
+//Bucle de animación
+function animationLoop() {
+  requestAnimationFrame(animationLoop);
+
+  //Modifica rotación de todos los objetos
+  for (let object of objetos) {
+    object.rotation.y += 0.01;
+  }
+
+  renderer.render(scene, camera);
+}
---
+++ IG/S9/code/script_33_shader_dibuja.js
@@ -0,0 +1,185 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+let scene, renderer;
+let camera;
+let info;
+let grid;
+let camcontrols;
+let uniforms;
+let objetos = [];
+
+init();
+animationLoop();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    20,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 0, 5);
+
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  document.body.appendChild(renderer.domElement);
+
+  //Objetos
+  uniforms = {
+    u_time: {
+      type: "f",
+      value: 1.0,
+    },
+    u_resolution: {
+      type: "v2",
+      value: new THREE.Vector2(),
+    },
+    u_mouse: {
+      type: "v2",
+      value: new THREE.Vector2(),
+    },
+  };
+
+  //Con color sólido
+  //Plane(5,2,0x0000ff)
+  //Con shader
+  PlaneShader(5, 2, fragmentShader_01());
+
+  camcontrols = new OrbitControls(camera, renderer.domElement);
+
+  //Dmensiones iniciales
+  onWindowResize();
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+//Redimensionado de la ventana
+function onWindowResize(e) {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  uniforms.u_resolution.value.x = renderer.domElement.width;
+  uniforms.u_resolution.value.y = renderer.domElement.height;
+}
+
+//Evento de ratón
+document.onmousemove = function (e) {
+  uniforms.u_mouse.value.x = e.pageX / window.innerWidth;
+  uniforms.u_mouse.value.y = e.pageY / window.innerHeight;
+};
+
+function PlaneShader(sx, sy, fragsh) {
+  let geometry = new THREE.PlaneBufferGeometry(sx, sy);
+  let material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    //Color sólido
+    fragmentShader: fragsh,
+    vertexShader: vertexShader(),
+  });
+
+  let mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  objetos.push(mesh);
+}
+
+function Plane(sx, sy, col) {
+  let geometry = new THREE.PlaneBufferGeometry(sx, sy);
+  let material = new THREE.ShaderMaterial({
+    color: col,
+  });
+
+  let mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  objetos.push(mesh);
+}
+
+//Bucle de visualización
+function animationLoop() {
+  requestAnimationFrame(animationLoop);
+
+  //Incrementa tiempo
+  uniforms.u_time.value += 0.05;
+
+  renderer.render(scene, camera);
+}
+
+function vertexShader() {
+  return `
+				varying vec3 vUv; 
+				varying vec4 modelViewPosition; 
+				
+				void main() {
+				  vUv = position; 
+				  vec4 modelViewPosition = modelViewMatrix * vec4(vUv, 1.0);
+				  gl_Position = projectionMatrix * modelViewPosition; 
+				}
+			  `;
+}
+
+function fragmentShader_01() {
+  return `
+					uniform vec2 u_resolution;
+					uniform float u_time;
+
+					void main() {
+					//Normalizamos coordenadas del píxel en base a la resolución
+					vec2 st = gl_FragCoord.xy/u_resolution;
+
+					//Escala de grises izquierda a derecha
+					gl_FragColor = vec4(vec3(st.x),1.0);
+					}
+			  `;
+}
+
+function fragmentShader_02() {
+  return `
+				uniform vec2 u_resolution;
+				uniform float u_time;
+
+				float grosor=0.1;
+
+				void main() {
+					vec2 st = gl_FragCoord.xy/u_resolution.xy;
+
+					vec3 color = vec3(st.x);	//Escala de grises izquierda a derecha;
+					// Línea de color verde en la diagonal   
+					if (st.y-grosor<st.x && st.x<st.y+grosor)
+						color = vec3(0.0,1.0,0.0);
+
+					gl_FragColor = vec4(color,1.0);
+				}
+			  `;
+}
+
+function fragmentShader_03() {
+  return `
+				uniform vec2 u_resolution;
+				uniform float u_time;
+
+				float grosor=0.1;
+
+				float plot(vec2 st, float pct){
+				//smoothstep da salida suave entre dos valores (Hermite)
+				//Combina dos para crear el "chichón"
+				return  smoothstep( pct-grosor, pct, st.y) -
+						smoothstep( pct, pct+grosor, st.y);
+				}
+
+				void main() {
+				  vec2 st = gl_FragCoord.xy/u_resolution.xy;
+
+				  vec3 color = vec3(st.x);	//Escala de grises izquierda a derecha;
+
+				  //Valor a tomar como referencia para bordes suaves
+				  float val = st.x;
+
+				  //Combina escala con línea según el valor de pct
+				  float pct = plot(st,val);
+          
+				  // Valores altos verde y bajos escala de grises
+				  color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+
+				  gl_FragColor = vec4(color,1.0);
+				}
+			  `;
+}
